d716286b45e25f58d549fa3a736ee94e034eb4af
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-29 19:01:27 -0300

    Setup api

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,5 +31,9 @@ module.exports = {
       { extensions: ['.jsx', '.js'] }
     ],
     'import/prefer-default-export': 'off',
+    'react/state-in-constructor': 'off',
+    'react/static-property-placement': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/prefer-stateless-function': 'off'
   },
 };
