07087ff5eb760509f34320dc762070da355f2a06
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-28 21:06:49 -0300

    Add global styles

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import Home from './Home';
-import Cart from './Cart';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
 
 export default function Routes() {
   return (
