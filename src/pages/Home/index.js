2855c9870fa2298e3d0eae4c46fa85bbb045494a
Lorena Calaça <lorena.calaca.od@gmail.com>
2019-10-29 17:01:28 -0300

    Style home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,5 +1,85 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe1.jpg?ims=326x"
+          alt="Tênis Adidas Falcon Feminino"
+        />
+        <strong>Tênis perfeito para performance</strong>
+        <span>R$ 209,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe1.jpg?ims=326x"
+          alt="Tênis Adidas Falcon Feminino"
+        />
+        <strong>Tênis perfeito para performance</strong>
+        <span>R$ 209,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe1.jpg?ims=326x"
+          alt="Tênis Adidas Falcon Feminino"
+        />
+        <strong>Tênis perfeito para performance</strong>
+        <span>R$ 209,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe1.jpg?ims=326x"
+          alt="Tênis Adidas Falcon Feminino"
+        />
+        <strong>Tênis perfeito para performance</strong>
+        <span>R$ 209,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-falcon-feminino/26/COL-4398-026/COL-4398-026_detalhe1.jpg?ims=326x"
+          alt="Tênis Adidas Falcon Feminino"
+        />
+        <strong>Tênis perfeito para performance</strong>
+        <span>R$ 209,99</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" /> 3
+          </div>
+          <span>Adicionar ao carrinho</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
